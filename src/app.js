a3d296d755379042e1e7765dbf8a29f0cee082b8
Everton Roberto <everton_roliveira@outlook.com>
2020-02-10 13:03:40 -0300

    config mongoose in app

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
 const exprees = require('express');
 const app = exprees();
 const http = require('http');
+const mongoose = require('mongoose');
+
 
 const port = normalizaPort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, async () => {
+    await mongoose.connect('mongodb://localhost:27017/db_angular_tdd', {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+        .then(() => {
+            console.log('DB connected!');
+            console.log(`API rodando porta ${port}`)
+        })
+        .catch(error => console.log(error));
+});
 
-console.log(`API rodando porta ${port}`);
 
 function normalizaPort(val) {
     const port = parseInt(val, 10);
