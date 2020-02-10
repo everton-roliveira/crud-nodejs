138b8ce20e818f04738e912170ffef16ce2b5202
Everton Roberto <everton_roliveira@outlook.com>
2020-02-10 11:01:49 -0300

    config server app

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const exprees = require('express');
+const app = exprees();
+const http = require('http');
+
+const port = normalizaPort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+
+console.log(`API rodando porta ${port}`);
+
+function normalizaPort(val) {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}
